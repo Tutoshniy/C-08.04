50369c2725816680121947721955eabe8c3226cc
Gleb <gleb.gadbrat@mail.ru>
2022-04-08 20:49:34 +0300

    Задание 2 - Найти произведение двух матриц.

---
+++ Task2/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество строк первой матрицы:" );
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов первой матрицы:" );
+int v = Convert.ToInt32(Console.ReadLine());
+int [,] matrix1 = new int [n,v];
+Console.WriteLine("Введите количество строк второй матрицы:" );
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов второй матрицы:" );
+int p = Convert.ToInt32(Console.ReadLine());
+int [,] matrix2 = new int [m,p];
+int [,] resultMatrix = new int [n, p];
+
+void PrintArray(int [,] mat)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            Console.Write($"{mat[i,j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int [,] mat)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            mat[i,j] = new Random().Next(1,101);
+        }
+    }
+}
+
+Console.WriteLine();
+FillArray (matrix1);
+PrintArray(matrix1);
+Console.WriteLine();
+Console.WriteLine(" * ");
+Console.WriteLine();
+FillArray(matrix2);
+PrintArray(matrix2);
+Console.WriteLine();
+Console.WriteLine(" = ");
+Console.WriteLine();
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        for (int z = 0; z < n; z++)
+        {
+            resultMatrix[i, z] += matrix1[i, j] * matrix2[j, z];
+        }
+    }
+}
+PrintArray(resultMatrix);
