dd2db91e386ca8593531cf27f0de4287fdc5203a
Gleb <gleb.gadbrat@mail.ru>
2022-04-08 20:00:52 +0300

    Задание 1 - Написать программу, которая в двумерном массиве заменяет строки на столбцы или сообщить, что это невозможно (в случае, если матрица не квадратная).

---
+++ Task1/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество строк:" );
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:" );
+int k = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = new int [n,k];
+
+void PrintArray(int [,] mat)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            Console.Write($"{mat[i,j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int [,] mat)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            mat[i,j] = new Random().Next(1,101);
+        }
+    }
+}
+
+if (n == k)
+{
+    FillArray(matrix);
+    Console.WriteLine("Данный массив: ");
+    PrintArray(matrix);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            temp = matrix[i, j];
+            matrix[i, j] = matrix [j, i];
+            matrix[j, i] = temp;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Получившийся массив: ");
+    PrintArray(matrix);
+}
+else
+{
+    Console.WriteLine("Невозможно поменять числа местами");
+}
